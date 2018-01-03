f7f56f86a8d220516a8be8fa832dbf80ed181c48
nikhil722sharma <nikhil722sharma@gmail.com>
2018-01-03 15:50:13 +0530

    testcommit

---
+++ Assets/JumpScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpScript : MonoBehaviour {
+
+    public GameObject cube;
+    public Vector3 jumpforce;
+	// Use this for initialization
+	void Start () {
+        jumpforce = new Vector3(0f, 400f, 0f);
+	}
+    private void OnTriggerEnter()
+    {
+        print("Collide");
+    }
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetButtonDown("Jump"))
+        {
+            cube.GetComponent<Rigidbody>().AddForce(jumpforce);
+        }
+	}
+}
